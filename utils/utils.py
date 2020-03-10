c7b60874382bbb51a558b9b466ea518a86bdf913
PotatoSpudowski <bahushruth.bahushruth@gmail.com>
2020-03-10 14:46:53 +0530

    Removed files

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 from skimage import io
+from efficientnet.keras import center_crop_and_resize, preprocess_input
 
 def verify_image(img_file):
     try:
@@ -7,12 +9,13 @@ def verify_image(img_file):
         return False
     return True
 
-def preprocess_image(image):
+def preprocess_image(image,image_size):
     image = imread(image)
-    image_size = model.input_shape[1]
     x = center_crop_and_resize(image, image_size=image_size)
     x = preprocess_input(x)
     x = np.expand_dims(x, 0)
 
     return x
+
+def get_image_vector_pair()
 
